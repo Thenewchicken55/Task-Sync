2dc22655f8b0e7b38040f292fbbc55c0d1e8400b
Thenewchicken55 <97483512+Thenewchicken55@users.noreply.github.com>
2025-05-23 08:27:22 -0400

    Skip the home page and go straight to calendar

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,28 +1,15 @@
-import React from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 
 export default function HomePage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/calendar");
+  }, []);
+  
   return (
-    <React.Fragment>
-      <Head>
-        <title>Home - Nextron (with-tailwindcss)</title>
-      </Head>
-      <div className="grid grid-col-1 text-2xl w-full text-center">
-        <div>
-          <Image
-            className="ml-auto mr-auto"
-            src="/images/logo.png"
-            alt="Logo image"
-            width={256}
-            height={256}
-          />
-        </div>
-      </div>
-      <div className="mt-1 w-full flex-wrap flex justify-center">
-        <Link href="/calendar">Go to next page</Link>
-      </div>
-    </React.Fragment>
+    <p>Redirecting ...</p>
   )
 }
